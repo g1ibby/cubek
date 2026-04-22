ec3e38a7cb7980303e370d343a02ffd351b3a6ff
Sublime Tshimpangila <tshimangasublime@gmail.com>
2026-04-22 12:23:28 -0400

    add simple test

--- crates/cubek-reduce/tests/it/reduce_dim.rs
+++ crates/cubek-reduce/tests/it/reduce_dim.rs
@@ -1,3 +1,6 @@
+use cubecl::zspace::{Shape, Strides};
+use cubek_reduce::{ReduceStrategy, launch::VectorizationStrategy, routines::{BlueprintStrategy, plane::PlaneStrategy}};
+
 use crate::it::test_case::TestCase;
 
 #[test]
@@ -23,6 +26,22 @@ pub fn test_topk_3() {
 #[test]
 pub fn test_topk_5() {
     test_case().test_topk(5);
+}
+
+#[test]
+pub fn test_plane_reduction_simple() {
+    let strategy = ReduceStrategy {
+        vectorization: VectorizationStrategy{ parallel_output_vectorization: true },
+        routine: RoutineStrategy::Plane(BlueprintStrategy::Inferred(PlaneStrategy{ independent:  true })),
+    };
+    let test_case = TestCase::new::<TestDType>(
+        Shape::new([16]),
+        Strides::new(&[1]),
+        Some(0),
+        strategy,
+    );
+
+    test_case.test_topk(3);
 }
 
 #[test]
